1eae9cec53b9948c0a5d6745c3495ab600942e83
Devang Bhalerao <144579330+Devang16-feb@users.noreply.github.com>
2025-01-28 23:55:12 +0530

    Add files via upload

---
+++ Set.py
@@ -0,0 +1,9 @@
+s = set() #Empty Set no reprtition allowed!
+s.add(1)
+s.add(2) # set = {1,2}
+
+s1 = {1,45,6}
+s2 = {7,8,1,78}
+
+print(s1.union(s2))  # non repetative numbers
+print(s1.intersection(s2)) #Common values 
